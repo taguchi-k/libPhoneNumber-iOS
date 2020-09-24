5da1d8aec961aa5ef818dc9143c6a3e7f8c47800
kentaro <taguchi-k@users.noreply.github.com>
2020-09-25 07:09:00 +0900

    fix demo

---
+++ libPhoneNumber-Demo/libPhoneNumber-Demo/libPhoneNumber-Demo-Bridging-Header.h
@@ -0,0 +1,7 @@
+// Manually added
+#import "NBPhoneNumberUtil.h"
+#import "NBPhoneNumber.h"
+
+// CocoaPods (check your library path)
+#import "libPhoneNumber_iOS/NBPhoneNumberUtil.h"
+#import "libPhoneNumber_iOS/NBPhoneNumber.h"
